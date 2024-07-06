18cc94657ad276238f34b6195f98fe7e6ffaa9bc
AdalOnShow <mdadalbadsha2008@gmail.com>
2024-07-06 08:50:06 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        plusJakarta: ['Plus Jakarta Sans'],
+        inter: ['Inter'],
+      },
+      colors: {
+        primary: ['#F44335'],
+        secondary: ['#F9CCC7'],
+      },
+    },
+  },
+  plugins: [],
+};
